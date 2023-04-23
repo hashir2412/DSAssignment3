9a0a3e86632f69a2226b18ed5e5cf67f206be61a
hashir2412 <hashir2412@gmail.com>
2023-04-22 20:00:51 -0500

    mu me le code for 1st TC failure

--- PhaseCommit/src/Client/Client.java
+++ PhaseCommit/src/Client/Client.java
@@ -14,6 +14,10 @@ public class Client {
       Scanner sc = new Scanner(System.in);
       boolean validate = true;
       while (validate) {
+        System.out.println("Enter 1. TC failure - Coordinator fails before sending \"prepare\" message");
+        System.out.println("Enter 2. Node failure - transaction coordinator does not receive \"yes\" from a node");
+        System.out.println("Enter 3. TC failure - Coordinator fails before sending \"prepare\" message");
+        System.out.println("Enter 4. TC failure - Coordinator fails before sending \"prepare\" message");
         String message = sc.nextLine();
         if (message.equals("close")) {
           validate = false;
--- PhaseCommit/src/Transaction_Coordinator/Communicate.java
+++ PhaseCommit/src/Transaction_Coordinator/Communicate.java
@@ -7,15 +7,32 @@ public class Communicate implements ICommunicate {
   @Override
   public void commit(String message) throws RemoteException {
     try {
-      TC.out.println("get()");
-      Thread.sleep(4000);
-      TC.out.println("prepare");
-      var res = TC.in.readLine();
-      if (res.equals("yes")) {
-        TC.out.println("commit");
-      } else {
-        System.out.println(res);
+      switch (message) {
+        case "1":
+          TC.out.println("get()");
+          Thread.sleep(4000);
+          TC.out.println("prepare");
+          var res = TC.in.readLine();
+          if (res.equals("yes")) {
+            TC.out.println("commit");
+          } else {
+            System.out.println(res);
+          }
+          break;
+        case "2":
+          TC.out.println("get()");
+          TC.out.println("prepare");
+          var res2 = TC.in.readLine();
+          if (res2.equals("yes")) {
+            TC.out.println("commit");
+          } else {
+            System.out.println(res2);
+          }
+          break;
+        default:
+          break;
       }
+
       if (TC.in.readLine().equals("Acknowledged")) {
         System.out.println("Transaction successfully committed");
       }
