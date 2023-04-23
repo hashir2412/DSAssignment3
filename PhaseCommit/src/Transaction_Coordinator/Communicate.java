83dcc4ba208b919818b8adbfe7ee8cd11f588f24
Arjit Madan <arjitmadan96@gmail.com>
2023-04-22 19:17:11 -0500

    Chutad le TC

--- PhaseCommit/src/Transaction_Coordinator/Communicate.java
+++ PhaseCommit/src/Transaction_Coordinator/Communicate.java
@@ -7,8 +7,15 @@ public class Communicate implements ICommunicate {
   @Override
   public void commit(String message) throws RemoteException {
     try {
-        TC.out.println(message + " commit");
-        System.out.println(TC.in.readLine());
+        TC.out.println("get()");
+        Thread.sleep(2000);
+        TC.out.println("prepare");
+        if(TC.in.readLine().equals("yes")){
+          TC.out.println("commit");
+        }
+        if(TC.in.readLine().equals("Acknowledged")){
+          System.out.println("Transaction successfully committed");
+        }
     } catch (Exception e) {
       // TODO: handle exception
       e.printStackTrace();
